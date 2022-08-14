798b5dbabb8295baa963c35a0a017a65afb27d66
Tony Pettigrew <tonypdev@Tonys-MacBook-Pro.local>
2022-08-14 16:24:17 -0700

    chore: Add types to a few route files

--- src/api/routes/api.ts
+++ src/api/routes/api.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from "express";
 
 const route = Router();
 
-export default (app: Router) => {
+export default (app: Router): void => {
   app.use("/", route);
 
   route.get("/", (req: Request, res: Response) => {
